19220fadf655bfa589dddb1ff1f673c416da31a4
Dedo-unuka <dzanazovko@gmail.com>
2022-04-04 21:40:28 +0200

    Adding ex10

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -2,7 +2,7 @@
 function convertStrToInt(str) {
     return parseInt(str);
 }
-console.log(convertStrToInt(44));
+
 console.log(convertStrToInt("44"));
 console.log(convertStrToInt("27"));
 console.log(convertStrToInt("JeffBezos"));
