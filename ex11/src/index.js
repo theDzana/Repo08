22536b5ee2a75f8bba657d9e6b96870eb669cb72
Dedo-unuka <dzanazovko@gmail.com>
2022-04-04 21:42:33 +0200

    Adding ex11

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -2,7 +2,7 @@
 function  convertToInt(str) {
     return parseInt(str, 16);
 }
-console.log(convertToInt("BA"));
+
 console.log(convertToInt("BA"));
 console.log(convertToInt("F1"));
 console.log(convertToInt("JeffBezos"));
