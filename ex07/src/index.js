8dd6979df1bfc3224a8a88f2501d1277b3bb9372
Dedo-unuka <dzanazovko@gmail.com>
2022-04-04 14:49:13 +0200

    Adding ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+var dress = "little dress";
+function myDress() {
+    var dress = "ball gown";
+    return dress;
+}
+console.log(myDress());
+// Only change code above this line
+
+module.exports = myDress;
