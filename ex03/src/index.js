6028f9955a1054083e7322690af86224b5c2e95d
Dedo-unuka <dzanazovko@gmail.com>
2022-04-04 12:27:37 +0200

    Adding ex 93

---
+++ ex03/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line  
+function localScope() {
+    var myVariable = "I am here!"
+    console.log(myVariable);
+}
+
+//Only change code above this line
+
+
+// myVariable is not defined outside of localScope
+localScope();
+
+if (typeof myVariable != "undefined") {
+    console.log("outside localScope", myVariable)
+} else {
+    console.log("outside localScope UNDEFINED!!!")
+}
+module.exports = localScope;
