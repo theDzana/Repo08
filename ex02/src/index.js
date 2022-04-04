d5de6e60dce9544d113b4cf848ef82575833b071
Dedo-unuka <dzanazovko@gmail.com>
2022-04-04 11:52:15 +0200

    Adding ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,8 @@
+// Write code beloww this line
+function functionPrintInfo(name, age) {
+    console.log(name, age);
+}
+functionPrintInfo ("John", 23);
+functionPrintInfo ("Michael", 43);
+// Write code above this line
+module.exports = functionPrintInfo;
