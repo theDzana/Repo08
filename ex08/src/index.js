f71cdaeb88a1bc9e6c8e22dc1546ea7877787482
Dedo-unuka <dzanazovko@gmail.com>
2022-04-04 21:32:33 +0200

    Ading ex08

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,7 +1,7 @@
 // Only change code beloww this line 
 var total = 0;
 function funcWithArg(digit) {
-    return (digit + 9) /5;
+    return (digit + 9) / 5;
 }
 total = funcWithArg(6);
 // Only change code above this line
